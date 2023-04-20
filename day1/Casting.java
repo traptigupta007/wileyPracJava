618b1ce8b3546ab9c1d65b74ab51ad5b1edcf583
traptigupta007 <traptigupta00712@gmail.com>
2023-04-20 14:53:40 +0530

    day1 commit

---
+++ day1/Casting.java
@@ -0,0 +1,9 @@
+public class Casting {
+    public static void main(String[] args) {
+        int myInt = 9;
+        double myDouble = myInt;
+        System.out.println(myDouble);
+        int myInt2 = (int) myDouble;
+        System.out.println(myInt2);
+    }
+}
---
+++ day1/varPrac.java
@@ -0,0 +1,7 @@
+public class varPrac {
+    public static void main(String args[]) {
+        int $dol = 9;
+        System.out.println($dol);
+    }
+
+}
