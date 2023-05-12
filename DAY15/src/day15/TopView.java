79928d9612c20ac7e9da43e1f40143dfb556eeb8
traptigupta007 <traptigupta00712@gmail.com>
2023-05-12 15:04:13 +0530

    day15 commit

---
+++ DAY15/src/day15/TopView.java
@@ -0,0 +1,39 @@
+package day15;
+
+import java.util.*;
+import java.util.TreeMap;
+class Pair{
+	Node n;
+	int lev;
+	Pair(Node n,int lev){
+		this.n=n;
+		this.lev=lev;
+	}
+}
+public class TopView {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeMap<Integer, Integer> h=new TreeMap<>();
+		Queue<Pair> q=new LinkedList<>();
+		Node t=new Node(6);
+		t.left=new Node(90);
+		t.left.left=new Node(11);
+		t.left.right=new Node(48);
+		t.left.right.right=new Node(15);
+		t.right=new Node(12);
+		t.right.left=new Node(18);
+		q.add(new Pair(t,0) );
+		while(!q.isEmpty()) {
+			Pair cur=q.poll();
+			int lev=cur.lev;
+			if(!h.containsKey(lev))
+				h.put(lev,cur.n.val);
+			if(cur.n.left!=null)
+				q.add(new Pair(cur.n.left,lev-1));
+			if(cur.n.right!=null)
+				q.add(new Pair(cur.n.right,lev+1));
+		}
+		System.out.println(h.values());
+	}
+
+}
---
+++ DAY15/src/day15/TopViewDFS.java
@@ -0,0 +1,32 @@
+package day15;
+
+import java.util.*;
+import java.util.TreeMap;
+
+public class TopViewDFS {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeMap<Integer, Integer> h=new TreeMap<>();
+//		Queue<Pair> q=new LinkedList<>();
+		Node t=new Node(8);
+		t.left=new Node(3);
+		t.left.left=new Node(1);
+		t.left.right=new Node(6);
+		t.left.right.left=new Node(4);
+		t.right=new Node(10);
+		t.right.right=new Node(14);
+		t.right.right.left=new Node(13);
+		dfs(0,t,h);
+		System.out.println(h.values());
+	}
+	public static void dfs(int lev,Node root,TreeMap<Integer, Integer> h) {
+		if(root==null)
+			return;
+		if(!h.containsKey(lev))
+			h.put(lev, root.val);
+		dfs(lev-1,root.left,h);
+		dfs(lev+1,root.right,h);
+		
+	}
+
+}
