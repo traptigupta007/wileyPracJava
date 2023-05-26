3b5c629b240198725cb5f81b1027519f248af042
traptigupta007 <traptigupta00712@gmail.com>
2023-05-26 09:09:41 +0530

    com

---
+++ misc/classpath/bookpub/src/main/java/com/wiley/bookpub/util/DBConnection.java
@@ -0,0 +1,16 @@
+package com.wiley.bookpub.util;
+import java.sql.*;
+import java.util.*;
+public class DBConnection {
+private DBConnection() {
+	
+}public static Connection getDBConnection() {
+	Connection dbcon=null;
+	ResourceBundle bundle=ResourceBundle.getBundle("db");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
